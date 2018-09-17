8aa692b231075ae09cdcbb94cc53ba32b2e6d439
TianJie <tianjie@Jies-MBA.local>
2018-09-17 23:37:15 +0800

    change server.js

--- server.js
+++ server.js
@@ -92,6 +92,7 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
+
 app.get('/counts', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
